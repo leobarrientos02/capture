e66f6c685069aeaaae6af399233c9beb4778aa82
Your Name <leobarrientos02@gmail.com>
2021-12-23 14:05:53 -0500

    little media query upgrade

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -59,7 +59,7 @@ const StyledNav = styled.nav`
         padding-left: 10rem;
         position: relative;
     }
-    @media (max-width: 1300px){
+    @media (max-width: 1500px){
         flex-direction: column;
         padding: 2rem 1rem;
         #Logo{
